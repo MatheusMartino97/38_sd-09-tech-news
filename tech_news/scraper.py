ed56c379c860e655f9deb5a59975ce75d0137ad6
Matheus Martino <matheus.martino97@gmail.com>
2021-11-16 08:11:48 -0300

    Fix req2.

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -17,82 +17,89 @@ def fetch(url):
         time.sleep(1)
 
 
-class News:
-    def get_url(self, selector):
-        url = selector.css("head link[rel=canonical]::attr(href)").get()
-        self.url = url
+def get_url(selector):
+    url = selector.css("head link[rel=canonical]::attr(href)").get()
+    return url
 
-    def get_title(self, selector):
-        title = selector.css(".tec--article__header__title::text").get()
-        self.title = title
 
-    def get_timestamp(self, selector):
-        timestamp_selector = ".tec--timestamp__item time::attr(datetime)"
-        timestamp = selector.css(timestamp_selector).get()
-        self.timestamp = timestamp
+def get_title(selector):
+    title = selector.css(".tec--article__header__title::text").get()
+    return title
 
-    def get_summary(self, selector):
-        summary_selector = "div.tec--article__body > p:nth-child(1) *::text"
-        summary = selector.css(summary_selector).getall()
-        self.summary = ''.join(summary)
 
-    def get_writer(self, selector):
-        selectors = [
-            ".tec--timestamp:nth-child(1) a::text",
-            ".tec--author__info p:first-child::text",
-            ".tec--author__info p:first-child a::text",
-        ]
-        selected = []
-        for curr_selector in selectors:
-            selected_writer = selector.css(curr_selector).get()
-            if selected_writer is not None:
-                selected.append(selected_writer.strip())
-            if selected_writer is None:
-                selected.append(None)
-        writer = [item for item in selected if item]
-        if len(writer) == 0:
-            return None
-        self.writer = writer[0]
+def get_timestamp(selector):
+    timestamp_selector = ".tec--timestamp__item time::attr(datetime)"
+    timestamp = selector.css(timestamp_selector).get()
+    return timestamp
 
-    def get_shares_count(self, selector):
-        shares = selector.css(".tec--toolbar div:first-child::text").get()
-        if shares is None or not ("Compartilharam") in shares:
-            return 0
-        shares_count = re.findall(r"\s(\d*)\s(...*)", shares)
-        self.shares_count = int(shares_count[0][0])
 
-    def get_comments_count(self, selector):
-        comments = selector.css("#js-comments-btn::attr(data-count)").get()
-        if comments is None:
-            return 0
-        self.comments_count = int(comments)
+def get_summary(selector):
+    summary_selector = "div.tec--article__body > p:nth-child(1) *::text"
+    summary = selector.css(summary_selector).getall()
+    return ''.join(summary)
 
-    def get_sources(self, selector):
-        sources = selector.css(".z--mb-16 .tec--badge::text").getall()
-        self.sources = [item.strip() for item in sources]
 
-    def get_categories(self, selector):
-        categories = selector.css("#js-categories a::text").getall()
-        self.cotegories = [item.strip() for item in categories]
+def get_writer(selector):
+    selectors = [
+        ".tec--timestamp:nth-child(1) a::text",
+        ".tec--author__info p:first-child::text",
+        ".tec--author__info p:first-child a::text",
+    ]
+    selected = []
+    for curr_selector in selectors:
+        selected_writer = selector.css(curr_selector).get()
+        if selected_writer is not None:
+            selected.append(selected_writer.strip())
+        if selected_writer is None:
+            selected.append(None)
+    writer = [item for item in selected if item]
+    if len(writer) == 0:
+        return None
+    return writer[0]
+
+
+def get_shares_count(selector):
+    shares = selector.css(".tec--toolbar div:first-child::text").get()
+    if shares is None or not ("Compartilharam") in shares:
+        return 0
+    shares_count = re.findall(r"\s(\d*)\s(...*)", shares)
+    return int(shares_count[0][0])
+
+
+def get_comments_count(selector):
+    comments = selector.css("#js-comments-btn::attr(data-count)").get()
+    if comments is None:
+        return 0
+    return int(comments)
+
+
+def get_sources(selector):
+    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
+    return [item.strip() for item in sources]
+
+
+def get_categories(selector):
+    categories = selector.css("#js-categories a::text").getall()
+    return [item.strip() for item in categories]
 
 
 # Requisito 2
 def scrape_noticia(html_content):
     selector = Selector(text=html_content)
 
-    news = News()
+    scraped_news = {
+        "url": get_url(selector),
+        "title": get_title(selector),
+        "timestamp": get_timestamp(selector),
+        "writer": get_writer(selector),
+        "shares_count": get_shares_count(selector),
+        "comments_count": get_comments_count(selector),
+        "summary": get_summary(selector),
+        "sources": get_sources(selector),
+        "categories": get_categories(selector),
+    }
 
-    news.get_url(selector)
-    news.get_title(selector)
-    news.get_timestamp(selector)
-    news.get_writer(selector)
-    news.get_shares_count(selector)
-    news.get_comments_count(selector)
-    news.get_summary(selector)
-    news.get_sources(selector)
-    news.get_categories(selector)
-
-    return news
+    return scraped_news
 
 
 # Requisito 3
