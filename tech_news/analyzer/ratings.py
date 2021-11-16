f38c39ad541e2e75abd9d55c242910e96f6f651c
Matheus Martino <matheus.martino97@gmail.com>
2021-11-16 12:27:47 -0300

    req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,22 @@
+from tech_news.database import find_news
+import operator
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    news_with_score = []
+
+    if (news):
+        for each_news in news:
+            each_news["score"] = news["shares_count"] + news["comments_count"]
+            news_with_score.append(each_news)
+
+    sorted_list = (
+        sorted(news_with_score, key=operator.itemgetter("score"), reverse=True)
+    )
+
+    return sorted_list[:5]
 
 
 # Requisito 11
