ce5e7dceb3d9dc9aee044b8e5f745d4b9bf8f304
Matheus Martino <matheus.martino97@gmail.com>
2021-11-16 11:31:37 -0300

    Req 7 e 8.

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,31 @@
+from tech_news.database import db
+import re
+
+
+def format_return(news):
+    return [(each_news["title"], each_news["url"]) for each_news in news]
+
+
+def validate_date_regex(date):
+    regex = re.search(r"(\d{4})-\d{2}-\d{2}", date)
+    if regex is None or int(regex[1]) < 2000:
+        raise ValueError("Data inválida")
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = db.news.find({"title": re.compile(title, re.IGNORECASE)})
+
+    return format_return(news)
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    validate_date_regex(date)
+
+    news = db.news.find({"timestamp": {"$regex": date}})
+
+    return format_return(news)
 
 
 # Requisito 8
